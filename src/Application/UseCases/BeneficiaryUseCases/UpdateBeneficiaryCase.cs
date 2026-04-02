9cf24806d28379995fc256f38f2f11b45eaa4d5f
eulucaslim <lucaslimacnt14@gmail.com>
2026-04-01 23:57:10 -0400

    fix: Update Validate Plan to verify if exists

--- src/Application/UseCases/BeneficiaryUseCases/UpdateBeneficiaryCase.cs
+++ src/Application/UseCases/BeneficiaryUseCases/UpdateBeneficiaryCase.cs
@@ -5,17 +5,32 @@ using Application.DTOs.Responses;
 using Application.Mappers;
 using Domain.Exceptions;
 using Domain.Repositories;
+using Domain.Validators;
 
 namespace Application.UseCases.BeneficiaryUseCases;
 
-public class UpdateBeneficiaryCase(IBeneficiaryRepository repository, BeneficiaryMapper mapper)
+public class UpdateBeneficiaryCase(
+    IBeneficiaryRepository repository, 
+    IPlanRepository planRepository, 
+    BeneficiaryMapper mapper)
     : IUpdateBeneficiaryCase
 {
     public async Task<BeneficiaryResponse> Handle(Guid id, BeneficiaryRequest request)
     {
         var beneficiary = await repository.GetByIdAsync(id);
         if (beneficiary == null) throw new EntityNotFound("The Beneficiary with id = " + id + " not found");
+        
+        // Validations
+        CpfValidator.IsValid(request.Cpf);
+        
+        var plan = await planRepository.GetByIdAsync(beneficiary.PlanId);
+        if (plan == null)
+        {
+            throw new EntityNotFound("The plan with id = " + beneficiary.PlanId + " not found");
+        }
+        
         mapper.UpdateEntity(beneficiary, request);
+        
         var beneficiaryUpdated = await repository.UpdateAsync(id, beneficiary);
 
         return new BeneficiaryResponse(
