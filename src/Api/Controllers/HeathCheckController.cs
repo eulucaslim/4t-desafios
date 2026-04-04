f1072d0fd8f85e0de8c636c21d9b990dc4a6d1c9
eulucaslim <lucaslimacnt14@gmail.com>
2026-04-04 12:12:33 -0400

    feat: Create a Helth Check router

---
+++ src/Api/Controllers/HeathCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("/")]
+[Produces("application/json")]
+public class HeathCheckController: ControllerBase
+{
+    [HttpGet]
+    public ActionResult<Dictionary<string,string>> Get()
+    {
+        var heath = new Dictionary<string, string> { { "status", "OK" }, { "datetime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") } };
+        return Ok(heath);
+    }
+}
